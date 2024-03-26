a9a855950b90a77d6284a7406ba7d3f763a61943
papercoding22 <john.ly997@gmail.com>
2024-03-26 21:51:20 +0700

    add: install React Query and setup

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,16 +1,21 @@
 import {NavigationContainer} from '@react-navigation/native';
 import * as React from 'react';
 import {Provider} from 'react-redux';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 import App from './App';
 import {store} from './store';
+
+const queryClient = new QueryClient();
 
 function Root() {
   return (
     <>
       <NavigationContainer>
         <Provider store={store}>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
         </Provider>
       </NavigationContainer>
     </>
