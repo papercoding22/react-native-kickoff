fb302cb53c160fef081288c9361949c4c3f813b6
papercoding22 <john.ly997@gmail.com>
2024-03-26 16:30:53 +0700

    add: basic setup Redux

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,13 +1,17 @@
 import {NavigationContainer} from '@react-navigation/native';
 import * as React from 'react';
+import {Provider} from 'react-redux';
 
 import App from './App';
+import {store} from './store';
 
 function Root() {
   return (
     <>
       <NavigationContainer>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </NavigationContainer>
     </>
   );
