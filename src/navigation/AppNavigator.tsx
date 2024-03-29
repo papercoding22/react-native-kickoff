8ffc34c2be90ef261e7328ef841f2b09b234cd5e
papercoding22 <john.ly997@gmail.com>
2024-03-29 21:43:57 +0700

    add: setup modal

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,9 +1,34 @@
 import * as React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import BottomTabNavigator from './BottomTabNavigator';
+import {Button, Text, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+const RootStack = createNativeStackNavigator();
+
+function ModalScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{fontSize: 30}}>This is a modal!</Text>
+      <Button onPress={() => navigation.goBack()} title="Dismiss" />
+    </View>
+  );
+}
 
 function AppNavigator() {
-  return <BottomTabNavigator />;
+  return (
+    <RootStack.Navigator>
+      <RootStack.Group screenOptions={{headerShown: false}}>
+        <RootStack.Screen name="Root" component={BottomTabNavigator} />
+      </RootStack.Group>
+      <RootStack.Group screenOptions={{presentation: 'modal'}}>
+        <RootStack.Screen name="MyModal" component={ModalScreen} />
+      </RootStack.Group>
+    </RootStack.Navigator>
+  );
 }
 
 export default AppNavigator;
