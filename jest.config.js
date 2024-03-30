3f9065c3f944586f9f578f9ec855bfad3e89c8ee
papercoding22 <john.ly997@gmail.com>
2024-03-30 14:07:41 +0700

    fix: jest cannot transform svg in test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.svg$': 'jest-transformer-svg',
+  },
   setupFilesAfterEnv: [
     './node_modules/react-native-gesture-handler/jestSetup.js',
     './src/test/jest-setup.tsx',
